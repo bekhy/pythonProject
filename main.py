053bb9a2b70a9f681506167ad70c2f604c1420ca
Ярослав <electro.bekh@gmail.com>
2021-03-03 09:27:38 +0200

    pos

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+# 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if input("Введите что-то: "):
+    print("OK")
+
+# 2
+number = int(input("Введите число: "))
+if number > 0:
+    print("1")
+elif number <= 0:
+    print("-1")
+
+# 3
+a = [2, 4, 6, 8, 12, 4245, 546, 40, 3234, 67, 45, 23]
+check = int(input("Введите число: "))
+if check in a:
+    print("Это число есть в массиве!")
+else:
+    print("Этого числа нет в массиве!")
+
+# 4
+dict = {"12":"A", "13":"B", "14":"C", "15":"D", "22":"E", "30":"F"}
+sear = input("Введите ключ по которому найти: ")
+if sear in dict:
+    print("Это ключ есть в словаре!")
+else:
+    print("Этого ключа нет в словаре!")
+
+# 1(1)
+paired = []
+unpaired = []
+other = []
+list = input("Введите список чисел: ")
+list =
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-hfjdrjhfgjftstjsf
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 2(2)
+# 3(3)
+# 4(4)
